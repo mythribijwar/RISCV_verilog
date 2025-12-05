e311b55cfe3cc11360cc26078d2edb11bdb8d941
mythribijwar <mythribijwar3@gmail.com>
2025-12-05 17:48:49 +0530

    Implement Control Unit for instruction decoding

--- Single_Cycle/Countrol_unit.v
+++ Single_Cycle/Countrol_unit.v
@@ -1,1 +1,17 @@
+// Control Unit
+// =======================
+module Control_Unit(instruction, Branch, MemRead, MemtoReg, MemWrite, ALUOp, ALUSrc, RegWrite);
+    input [6:0] instruction;
+    output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
+    output reg [1:0] ALUOp;
+
+    always @(*) begin
+        case (instruction)
+            7'b0110011: {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b100100_01;
+            7'b0000011: {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b111110_00;
+            7'b0100011: {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b100010_00;
+            7'b1100011: {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} <= 8'b000001_01;
+        endcase
+    end
+endmodule
 
